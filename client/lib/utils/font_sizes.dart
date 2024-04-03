20c25a6b58c0cab19fbda01c78ef952e76798dbb
mohsin-mw <null.mmw@gmail.com>
2024-04-03 21:21:44 +0500

    Style: Create Colors and Font Size files

---
+++ client/lib/utils/font_sizes.dart
@@ -0,0 +1,7 @@
+class AppTextStyles {
+  // Text Sizes
+  static const double textSizeSmall = 12;
+  static const double textSizeMedium = 16;
+  static const double textSizeLarge = 20;
+  static const double textSizeExtraLarge = 34;
+}
