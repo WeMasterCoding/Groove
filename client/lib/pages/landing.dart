0890b3dfd0204a7eed9b2c604a7f7ee36946092d
Mohsin <122507740+Mohsin-mw@users.noreply.github.com>
2024-04-03 21:26:45 +0500

    Refactor: Cleaned Landing Page

--- client/lib/pages/landing.dart
+++ client/lib/pages/landing.dart
@@ -1,128 +1,125 @@
-import 'package:flutter/cupertino.dart';
+import 'package:client/utils/colors.dart';
+import 'package:client/utils/font_sizes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Landing extends StatelessWidget {
   const Landing({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF050B16),
+      backgroundColor: AppColors.accent,
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30),
         child: Column(
-          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            const Image(
-              image: AssetImage('images/LandingSplashOne.png'),
-              height: 440,
-              fit: BoxFit.cover,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.symmetric(
-                  horizontal: 25, vertical: 20),
-              child: Column(
-                children: [
-                  const Text(
-                    "Unleash Your Creativity with Ai Artistry",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 34,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Explore a world where imagination meets innovation at the tap of your finger",
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.5),
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.1),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.symmetric(
-                  horizontal: 25, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: null,
-                      style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                          const EdgeInsets.symmetric(
-                              horizontal: 20.0,
-                              vertical: 15.0), // Adjust padding
-                        ),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF35383F), // Background color
-                        ),
-                      ), // Replace with your desired action
-                      child: const Text(
-                        "Skip",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white, // Text color
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: null,
-                      style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                          const EdgeInsets.symmetric(
-                              horizontal: 20.0,
-                              vertical: 15.0), // Adjust padding
-                        ),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xff008cff), // Background color
-                        ),
-                      ), // Replace with your desired action
-                      child: const Text(
-                        "Next",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white, // Text color
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            _buildImage(),
+            _buildDivider(),
+            _buildContent(),
+            _buildDivider(),
+            _buildButtons(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return SizedBox(
+      height: 440,
+      child: Image.asset(
+        'images/LandingSplashOne.png',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white.withOpacity(0.1),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+      child: Column(
+        children: [
+          const Text(
+            "Unleash Your Creativity with Ai Artistry",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: AppTextStyles.textSizeExtraLarge,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            "Explore a world where imagination meets innovation at the tap of your finger",
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.5),
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 15.0,
+                ),
+                backgroundColor: AppColors.secondary,
+              ),
+              child: const Text(
+                "Skip",
+                style: TextStyle(
+                  fontSize: AppTextStyles.textSizeMedium,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 15.0,
+                ),
+                backgroundColor: AppColors.primaryColor,
+              ),
+              child: const Text(
+                "Next",
+                style: TextStyle(
+                  fontSize: AppTextStyles.textSizeMedium,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
