2ad68b1616bafa1ec311b2b6b9f996670e34ddf1
Mohsin <122507740+Mohsin-mw@users.noreply.github.com>
2024-04-03 21:23:00 +0500

    Style: Create Colors and Font Size files

---
+++ client/lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff008cff);
+  static const Color secondary = Color(0xFF35383F);
+  static Color tertiary = Colors.white.withOpacity(0.1);
+  static const Color accent = Color(0xFF050B16);
+}
