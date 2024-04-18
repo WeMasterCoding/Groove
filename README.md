4185e684cee600f048fd5d4bfbe3397a870dc793
sameer <tenyson2005@gmail.com>
2024-04-19 00:16:32 +0530

    home and room page

--- README.md
+++ README.md
@@ -1,32 +1,37 @@
 # Groove - Watch Parties & Chat
 
+![Thumbnail](https://res.cloudinary.com/dvjzuiyp1/image/upload/v1713465816/Untitled_wuzu5y.png)
+![Thumbnail](https://res.cloudinary.com/dvjzuiyp1/image/upload/v1713465862/Untitled_ze1b9l.png)
+
 ## Overview
+
 Groove is your ultimate destination for hosting watch parties with friends while chatting in real-time! Say goodbye to solo movie nights and enjoy the top TV shows and movies from Netflix, Disney+, YouTube, Amazon Prime Video, HBO MAX, and more, all while socializing with your buddies.
 
 ## 🚀 Key Features
+
 - **Stream Together:** Choose from popular streaming platforms like Netflix, Disney+, Amazon Prime Video, HBO Max, YouTube, and others to watch your favorite content with friends.
 - **Real-Time Chat:** Chat with your friends seamlessly while watching TV shows and movies together, making the experience more interactive and fun.
 - **Easy Invitations:** Invite your friends to join your watch party with just a few taps, making it effortless to sync up and enjoy entertainment together.
 - **Multi-Platform Support:** Groove supports various streaming platforms, ensuring compatibility with your preferred content providers.
 - **Intuitive Interface:** Navigate through the app's user-friendly interface designed to enhance your watch party experience.
 
-## 🛠️ Technologies Used
-- **Frontend:** Flutter
-- **Backend:** (If applicable, mention any backend technologies used)
-- **Database:** (If applicable, mention any database technologies used)
-
 ## 🌟 Getting Started
+
 Start hosting watch parties with Groove in no time:
+
 1. Clone the repository: `https://github.com/WeMasterCoding/Groove`
 2. Install dependencies and set up the project.
 3. Customize the app to your preferences.
 4. Invite your friends and start enjoying watch parties together!
 
 ## 🤝 Contribution Guidelines
+
 Contributions to Groove are encouraged! Please refer to the contribution guidelines in the project repository for more details on how to contribute.
 
 ## 📄 License
+
 Groove operates under the [MIT License](https://github.com/WeMasterCoding/Groove/LICENSE.txt). Refer to the LICENSE file for more information.
 
 ## 📧 Contact
+
 For any inquiries or support regarding Groove, feel free to reach out via email at [wemastercoding@gmail.com](mailto:wemastercoding@gmail.com). Join our [Discord server](https://discord.gg/A7cz3dtpFD) for real-time discussions and updates.
--- client/lib/pages/home/home.dart
+++ client/lib/pages/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:groove/main.dart';
 import 'package:groove/routes/route.dart';
+import 'package:groove/utils/colors.dart';
 import 'package:groove/widgets/input_field.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
@@ -16,6 +17,7 @@ class _HomeState extends State<Home> {
   TextEditingController joinRoomID = TextEditingController();
   late IO.Socket socket;
   late String? roomID;
+  late String? userEmail;
 
   @override
   void initState() {
@@ -23,9 +25,11 @@ class _HomeState extends State<Home> {
 
     print('@# supabase currentUser: ');
     print(supabase.auth.currentUser);
+    userEmail = supabase.auth.currentUser?.email;
 
     setState(() {
-      roomID = supabase.auth.currentUser?.id;
+      // roomID = supabase.auth.currentUser?.id;
+      roomID = supabase.auth.currentUser?.email;
     });
 
     print('home page rendered');
@@ -41,41 +45,246 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            child: ElevatedButton(
-              onPressed: () async {
-                await supabase.auth.signOut();
-                await GoogleSignIn().signOut();
-                print('signout sucessfull');
-              },
-              child: Text('Logout'),
+      //   appBar: AppBar(
+      //     title: const Text('Home Page'),
+      //   ),
+      //   body: Column(
+      //     children: [
+      //       Container(
+      //         child: ElevatedButton(
+      //           onPressed: () async {
+      //             await supabase.auth.signOut();
+      //             await GoogleSignIn().signOut();
+      //             print('signout sucessfull');
+      //           },
+      //           child: Text('Logout'),
+      //         ),
+      //       ),
+      //       ElevatedButton(
+      //           onPressed: () => createRoom(context, socket, roomID),
+      //           child: const Text('create room')),
+      //       Container(
+      //         color: Colors.red,
+      //         child: InputField(
+      //             fieldName: 'room id',
+      //             icon: const Icon(Icons.room_outlined),
+      //             controller: joinRoomID),
+      //       ),
+      //       ElevatedButton(
+      //           onPressed: () => joinRoom(context, socket, joinRoomID.text),
+      //           child: const Text('Join room')),
+      //       Text(roomID!),
+      //       ElevatedButton(
+      //           onPressed: () {
+      //             socket.emit('checkRooms');
+      //           },
+      //           child: Text("log available rooms"))
+      //     ],
+      //   ),
+
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        padding: EdgeInsets.all(20),
+        color: AppColors.accent,
+        height: MediaQuery.of(context).size.height,
+        // color: Colors.black,
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Image.asset('images/Logo.png'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Welcome to Groove',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Email: $userEmail",
+                  style: const TextStyle(
+                      color: Colors.white54,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w900),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      "Room ID",
+                      style: TextStyle(
+                        color: Color.fromARGB(228, 255, 255, 255),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+                TextField(
+                  controller: joinRoomID,
+                  style: TextStyle(
+                    color: Colors.cyan[200],
+                    fontSize: 20,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: "user@gmail.com",
+                    hintStyle: TextStyle(
+                      color: Colors.white54,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 19,
+                    ),
+                    // contentPadding: EdgeInsets.all(10),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white60,
+                        width: 1.2,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 255, 81, 0),
+                        width: 1.2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 45,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () => joinRoom(context, socket, joinRoomID.text),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(AppColors.primaryColor)),
+                    child: const Text(
+                      'JOIN ROOM',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Color.fromARGB(204, 255, 255, 255),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Center(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 0.9,
+                          decoration: const BoxDecoration(
+                            color: Colors.white38,
+                          ),
+                        ),
+                      ),
+                      const Center(
+                        child: Expanded(
+                          child: Text(
+                            '   OR   ',
+                            style: TextStyle(
+                              color: Colors.white38,
+                              fontWeight: FontWeight.w900,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 0.9,
+                          decoration: const BoxDecoration(
+                            color: Colors.white38,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+
+                // CREATE ROOM BUTTON
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        AppColors.primaryColor,
+                      ),
+                    ),
+                    onPressed: () => createRoom(context, socket, roomID),
+                    child: const Text(
+                      'CREATE ROOM',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(204, 255, 255, 255),
+                      ),
+                    ),
+                  ),
+                ),
+                // const SizedBox(
+                //   height: 5,
+                // ),
+                // Text(
+                //   'Your Room ID will be your Email',
+                //   style: TextStyle(color: Colors.blue[200]),
+                // ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      // shadowColor: MaterialStateColor,
+                      backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 255, 47, 0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      await supabase.auth.signOut();
+                      await GoogleSignIn().signOut();
+                      print('signout sucessfull');
+                    },
+                    child: const Text(
+                      'LOG OUT',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 225, 225, 225)),
+                    ),
+                  ),
+                ),
+                // ElevatedButton(
+                //     onPressed: () {
+                //       socket.emit('checkRooms');
+                //     },
+                //     child: Text("log available rooms"))
+              ],
             ),
           ),
-          ElevatedButton(
-              onPressed: () => createRoom(context, socket, roomID),
-              child: const Text('create room')),
-          Container(
-            color: Colors.red,
-            child: InputField(
-                fieldName: 'room id',
-                icon: const Icon(Icons.room_outlined),
-                controller: joinRoomID),
-          ),
-          ElevatedButton(
-              onPressed: () => joinRoom(context, socket, joinRoomID.text),
-              child: const Text('Join room')),
-          Text(roomID!),
-          ElevatedButton(
-              onPressed: () {
-                socket.emit('checkRooms');
-              },
-              child: Text("log available rooms"))
-        ],
+        ),
       ),
     );
   }
